1c6bfe03711cf80bc91fd2e8da02f0766106dac3
Khris <kaw0115@auburn.edu>
2022-12-27 13:10:57 -0600

    add playRound function to initate a game

--- script.js
+++ script.js
@@ -5,7 +5,35 @@ const getComputerChoice = () => {
     const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)]
 }
-console.log(getComputerChoice())
+
+
+const playRound = (player, compChoice) => {
+    let playerChoice = player.toLowerCase();
+    //Draw
+    if (playerChoice === compChoice) {
+        return 'This game was a draw'
+    }
+
+    //playerChoice = scissors
+    if (playerChoice === 'scissors' && compChoice === 'paper') {
+        return 'You Win! Scissors beats Paper'
+    }else if (playerChoice === 'scissors' && compChoice === 'rock'){
+        return 'You Lose! Rock beats Scissors'
+    }
+    //playerChoice = rock
+    if (playerChoice === 'rock' && compChoice === 'scissors') {
+        return 'You Win! Rock beats Scissors'
+    }else if (playerChoice === 'rock' && compChoice === 'paper'){
+        return 'You Lose! Paper beats Rock'
+    }
+    //playerChoice = paper
+    if (playerChoice === 'paper' && compChoice === 'rock') {
+        return 'You Win! Paper beats Rock'
+    }else if (playerChoice === 'paper' && compChoice === 'scissors'){
+        return 'You Lose! Scissors beats Paper'
+    }
+   
+}
 
 
 
