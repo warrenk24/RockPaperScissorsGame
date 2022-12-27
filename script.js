5d92c8cf697fc2f555d5be519047ceba83d39b95
Khris <kaw0115@auburn.edu>
2022-12-27 14:48:21 -0600

    add score counter

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let playerCount = 0;
+let compCount = 0
 
 // This function will randomly choose rock, paper, or scissors
 const getComputerChoice = () => {
@@ -16,24 +17,46 @@ const playRound = (player, compChoice) => {
 
     //playerChoice = scissors
     if (playerChoice === 'scissors' && compChoice === 'paper') {
+        playerCount++;
         return 'You Win! Scissors beats Paper'
     }else if (playerChoice === 'scissors' && compChoice === 'rock'){
+        compCount++;
         return 'You Lose! Rock beats Scissors'
     }
     //playerChoice = rock
     if (playerChoice === 'rock' && compChoice === 'scissors') {
+        playerCount++;
         return 'You Win! Rock beats Scissors'
     }else if (playerChoice === 'rock' && compChoice === 'paper'){
+        compCount++;
         return 'You Lose! Paper beats Rock'
     }
     //playerChoice = paper
     if (playerChoice === 'paper' && compChoice === 'rock') {
+        playerCount++;
         return 'You Win! Paper beats Rock'
     }else if (playerChoice === 'paper' && compChoice === 'scissors'){
+        compCount++;
         return 'You Lose! Scissors beats Paper'
     }
    
 }
+//Will play five games total and pick the winner
+const game = () => {
 
+for (let i = 0; i < 5; i++){
+    console.log(playRound('rock', getComputerChoice()))    
+}
 
+if (playerCount === compCount){
+    return 'Draw!'
+}else if (playerCount > compCount){
+    return 'You Won the game!'
+}else {
+    return 'You Lost the game!'
+}
+
+}
+
+console.log(game())
 
